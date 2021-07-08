79df0174055edb70c2be63e06d33d593276518b9
Zak Gogi <zakgogi1@gmail.com>
2021-07-08 11:18:38 +0100

    changed port to env variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // starts the server seperately from backend on port 3000
 const server = require("./app.js");
-const port = 3000;
+const port = process.env.PORT;
 
 server.listen(port, () => console.log("Starting up the server"));
